2968a774bfb1ad425a27ccf3e6f4f98610cb4136
Caryl Marie <2161397@slu.edu.ph>
2019-05-23 17:13:56 +0800

    added fillouthauling.php

---
+++ Materials Engineer/fillouthauling.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>NGCBDC</title>
+    <link rel="icon" type="image/png" href="Images/NGCB_logo.png">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</head>
+
+<body class="sample">
+    <div class="mx-auto col-md-9">
+        <div class="card">
+            <div class="card-header">
+                <h4>Hauling Form</h4>
+            </div>
+            <div class="card-body">
+                <form class="form">
+                    <div class="row date-container">
+                        <div class="col-md-12 ">
+                            <label>Date:</label>
+                            <div class="col-lg-12">
+                                <input class="form-control" type="date" value="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row container">
+                        <div class="form-group">
+                            <label">Delivered to</label>
+                            <div class="col-lg-6">
+                                <input class="form-control" type="text" value="">
+                            </div>
+                            <label>Hauled From</label>
+                            <div class="col-lg-4">
+                                <input class="form-control" type="text" value="">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
