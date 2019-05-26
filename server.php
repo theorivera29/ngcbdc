710f0f2d9019cdffdcaad74bef64b5eaade51ba6
Theo Rivera <theorivera29.tmk@gmail.com>
2019-05-27 00:07:58 +0800

    Dashboard

--- server.php
+++ server.php
@@ -13,23 +13,23 @@
         $stmt->store_result();
         $stmt->bind_result($accounts_id, $accounts_password, $accounts_type);
         $stmt->fetch();
-        if(password_verify($password, $accounts_password)) {
+        if (password_verify($password, $accounts_password)) {
             $_SESSION['account_id']= $accounts_id;
             $_SESSION['loggedin' ] = true;
             $_SESSION['account_type'] = $accounts_type;
             if (strcmp($accounts_type,"Admin") == 0) {
-                header("location: http://127.0.0.1/NGCB/Admin/admindashboard.php");
+                header("location: http://127.0.0.1/NGCBDC/Admin/admindashboard.php");
                 $stmt->close();                
             } else if (strcmp($accounts_type,"Materials Engineer") == 0) {
-                header("location: http://127.0.0.1/NGCB/Materials%20Engineer/dashboard.php");    
+                header("location: http://127.0.0.1/NGCBDC/Materials%20Engineer/dashboard.php");    
                 $stmt->close();                            
             } else {
-                header("location: http://127.0.0.1/NGCB/View%20Only/projects.php");
+                header("location: http://127.0.0.1/NGCBDC/View%20Only/projects.php");
                 $stmt->close();                                
             }
         } else {
             $_SESSION['login_error'] = true;
-            header("location: http://127.0.0.1/NGCB/index.php");
+            header("location: http://127.0.0.1/NGCBDC/index.php");
             $stmt->close();                
         } 
     }
@@ -38,6 +38,48 @@
 
     
 // <--Materials Engineer-->
+    if (isset($_POST['create_todo'])) {
+        session_start();
+        $account_id = "";
+        if(isset($_SESSION['account_id'])) {
+            $account_id = $_SESSION['account_id'];
+        }
+        $todo_date = mysqli_real_escape_string($conn, $_POST['todo_date']);
+        $todo_task = mysqli_real_escape_string($conn, $_POST['todo_task']);
+        $todo_status = "in progress";
+        $stmt = $conn->prepare("INSERT INTO todo (todo_date, todo_task, todo_status, todoOf) VALUES (?, ?, ?, ?);");
+        $stmt->bind_param("sssi", $todo_date, $todo_task, $todo_status, $account_id);
+        $stmt->execute();
+        $stmt->close();
+        $create_todo_date = date("Y-m-d G:i:s");
+        $logs_message = 'Created todo task';
+        $logs_of = $account_id;
+        $stmt = $conn->prepare("INSERT INTO logs (logs_datetime, logs_activity, logs_logsOf) VALUES (?, ?, ?);");
+        $stmt->bind_param("ssi", $create_todo_date, $logs_message, $logs_of);
+        $stmt->execute();
+        $stmt->close();
+        header("Location:http://127.0.0.1/NGCBDC/Materials%20Engineer/dashboard.php");     
+    }
+
+    if (isset($_POST['update_todo'])) {
+        $todo_id = $_POST['todo_id'];
+        $todo_status = $_POST['todo_status'];
+        $todo_task = $_POST['todo_task'];
+        session_start();
+        $account_id = "";
+        if(isset($_SESSION['account_id'])) {
+            $account_id = $_SESSION['account_id'];
+        }
+        $update_todo_date = date("Y-m-d G:i:s");
+        if (strcmp($todo_status, "in progress") == 0) {
+            mysqli_query($conn, "UPDATE todo SET todo_status = 'done' WHERE todo_id = $todo_id");
+            mysqli_query($conn, "INSERT INTO logs (logs_datetime, logs_activity, logs_logsOf) VALUES ('$update_todo_date', 'Updated todo task '.$todo_task.' to done', $account_id);");
+        } else {
+            mysqli_query($conn, "DELETE FROM todo WHERE todo_id = $todo_id");
+            mysqli_query($conn, "INSERT INTO logs (logs_datetime, logs_activity, logs_logsOf) VALUES ('$update_todo_date', 'Cleared todo task '.$todo_task, $account_id);");
+        }
+        header("location: http://127.0.0.1/NGCBDC/Materials%20Engineer/dashboard.php");    
+    }
     
     if (isset($_POST['create_hauling'])) {
         $hauling_no = mysqli_real_escape_string($conn, $_POST['formNumber']);
@@ -78,7 +120,7 @@
         $stmt->close();
         $account_id = "";
         session_start();
-        if(isset($_SESSION['account_id'])) {
+        if (isset($_SESSION['account_id'])) {
             $account_id = $_SESSION['account_id'];
         }
         $stmt = $conn->prepare("INSERT INTO logs (logs_datetime, logs_activity, logs_logsOf) VALUES (?, ?, ?);");
@@ -88,7 +130,7 @@
         $logs_of = $account_id;
         $stmt->execute();
         $stmt->close();
-        header("Location:http://127.0.0.1/NGCB/Materials%20Engineer/hauleditems.php");        
+        header("Location:http://127.0.0.1/NGCBDC/Materials%20Engineer/hauleditems.php");        
     }
 
     
