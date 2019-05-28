c4394e0281beae04e358e9d50e9715d421198357
Caryl Marie <2161397@slu.edu.ph>
2019-05-28 23:54:12 +0800

    update forms

--- Materials Engineer/disposalslip.php
+++ Materials Engineer/disposalslip.php
@@ -24,7 +24,7 @@
                         <div class="col-lg-12">
                             <label class="col-lg-12 col-form-label">Date:</label>
                             <div class="col-lg-12">
-                                <input class="form-control" type="date" name="requisitionDate">
+                                <input class="form-control" type="date" name="disposalDate">
                             </div>
                         </div>
                     </div>
