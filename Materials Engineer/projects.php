1a8fa7ae0a6934e0deb2d04985e8d0efa174685f
jamspicaa <Jamspica25@gmail.com>
2019-05-30 13:54:39 +0800

    Update projects.php
    Fixed tabs conflict

--- Materials Engineer/projects.php
+++ Materials Engineer/projects.php
@@ -6,10 +6,12 @@
     <title>NGCBDC</title>
     <link rel="icon" type="image/png" href="Images/NGCB_logo.png">
     <link rel="stylesheet" type="text/css" href="../style.css">
+    
     <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
-    <script src="../JS/jquery/jquery-3.4.1.min.js"></script>
-    <script src="../JS/popper/popper.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="../bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
@@ -17,14 +19,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12 project-tabs">
-                    <nav>
                         <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
                             <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
                                 role="tab" aria-controls="nav-home" aria-selected="true">ONGOING</a>
                             <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
                                 role="tab" aria-controls="nav-profile" aria-selected="false">CLOSED</a>
                         </div>
-                    </nav>
+                
                 </div>
                 <div class="project-tabs-content">
                     <div class="tab-content" id="nav-tabContent">
