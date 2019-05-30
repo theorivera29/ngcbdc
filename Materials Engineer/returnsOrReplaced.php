c0c91eeab76803433c161cc3c86adbff6c025aee
jamspicaa <Jamspica25@gmail.com>
2019-05-30 14:40:27 +0800

    Create returns or replaced
    Merge the content of materialstobereplaced and materialstobereturn

---
+++ Materials Engineer/returnsOrReplaced.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>NGCBDC</title>
+    <link rel="icon" type="image/png" href="Images/NGCB_logo.png">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="../bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <div id="content">
+        <span class="slide">
+            <a href="#" class="open" id="sideNav-a" onclick="openSlideMenu()">
+                <i class="fas fa-bars"></i>
+            </a>
+            <h4 class="title">NEW GOLDEN CITY BUILDERS AND DEVELOPMENT CORPORATION</h4>
+            <!-- Example single danger button -->
+            <div class="btn-group dropdown-account">
+                <button type="button" class="btn dropdown-toggle dropdown-settings" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                </button>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" id="sideNav-a" href="account.php">Account Settings</a>
+                    <a class="dropdown-item" id="sideNav-a" href="">Logout</a>
+                </div>
+            </div>
+        </span>
+
+        <div id="menu" class="navigation sidenav">
+            <a href="#" class="close" id="sideNav-a" onclick="closeSlideMenu()">
+                <i class="fas fa-times"></i>
+            </a>
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <h3>NGCBDC</h3>
+                </div>
+
+                <ul class="list-unstyled components">
+                    <li>
+                        <a href="dashboard.php" id="sideNav-a">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <a href="#siteSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
+                            id="sideNav-a">Site</a>
+                        <ul class="collapse list-unstyled" id="siteSubmenu">
+                            <li>
+                                <a href="projects.php" id="sideNav-a">Projects</a>
+                            </li>
+                            <li>
+                                <a href="sitematerials.php" id="sideNav-a">Site Materials</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="active">
+                        <a href="#haulingSebmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
+                            id="sideNav-a">Hauling</a>
+                        <ul class="collapse list-unstyled" id="haulingSebmenu">
+                            <li>
+                                <a href="fillouthauling.php" id="sideNav-a">Fill out Hauling Receipt</a>
+                            </li>
+                            <li>
+                                <a href="hauleditems.php" id="sideNav-a">View Hauled Materials</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="" id="sideNav-a">Returns/Replacements</a>
+                    </li>
+                    <li>
+                        <a href="reports.php" id="sideNav-a">Reports</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+
+    </div>
+
+    <section id="tabs">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 project-tabs">
+                    <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
+                            role="tab" aria-controls="nav-home" aria-selected="true">RETURN</a>
+                        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
+                            role="tab" aria-controls="nav-profile" aria-selected="false">REPLACE</a>
+                    </div>
+
+                </div>
+                <div class="returns-or-replace-content">
+                    <div class="tab-content" id="nav-tabContent return-container">
+                        <div class="tab-pane fade show active" id="nav-home" role="tabpanel"
+                            aria-labelledby="nav-home-tab">
+                            <div class="card return-container">
+                                <div class="card-header">
+                                    <h4>List of Materials to be Return</h4>
+                                </div>
+                                <table class="table hauled-items-table table-striped table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Form No.</th>
+                                            <th scope="col">Hauling Date</th>
+                                            <th scope="col">Hauled From</th>
+                                            <th scope="col">Hauled By</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td><button type="button" class="btn btn-success">Open</button></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                            <div class="card replace-container">
+                                <div class="card-header">
+                                    <h4>List of Materials to be Replaced</h4>
+                                </div>
+                                <table class="table hauled-items-table table-striped table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Disposal Date</th>
+                                            <th scope="col">Project</th>
+                                            <th scope="col">Location</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td><button type="button" class="btn btn-success">Open</button></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+<script>
+    function openSlideMenu() {
+        document.getElementById('menu').style.width = '15%';
+    }
+
+    function closeSlideMenu() {
+        document.getElementById('menu').style.width = '0';
+        document.getElementById('content').style.marginLeft = '0';
+    }
+
+    $(document).ready(function () {
+
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+
+    });
+</script>
+
+</html>
