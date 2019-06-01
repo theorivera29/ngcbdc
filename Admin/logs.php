21877203425b6e4c55c4cdf6166d9c69509af9f0
jamspicaa <Jamspica25@gmail.com>
2019-06-02 04:03:37 +0800

    ADMIN
    list of accounts
    logs
    password request
    projects

--- Admin/logs.php
+++ Admin/logs.php
@@ -1,25 +1,38 @@
 <?php
     include "../db_connection.php";
+    session_start();
+
+    $accounts_id = $_SESSION['account_id'];    
 ?>
+
 <!DOCTYPE html>
 
 <html>
 
 <head>
     <title>NGCBDC</title>
-    <link rel="icon" type="image/png" href="Images/NGCB_logo.png">
-    <link rel="stylesheet" type="text/css" href="../style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="../Images/login2.png">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
         integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script src="../JS/jquery/jquery-3.4.1.min.js"></script>
+    <script src="../bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
 </head>
 
 <body>
-<div id="content">
+    <div id="content">
         <span class="slide">
             <a href="#" class="open" id="sideNav-a" onclick="openSlideMenu()">
                 <i class="fas fa-bars"></i>
@@ -83,9 +96,8 @@
             </nav>
         </div>
     </div>
-    <div class="card list-of-accounts-container">
-        <h5 class="card-header">List of Accounts</h5>
-        <table class="table list-of-accounts-table table-striped table-bordered">
+    <div class="list-of-accounts-container">
+        <table class="table list-of-accounts-table table-striped table-bordered" id="mydatatable">
             <thead>
                 <tr>
                     <th scope="col">Date</th>
@@ -102,27 +114,29 @@
                     <td></td>
                 </tr>
             </tbody>
+
         </table>
-    </div>
     </div>
 </body>
 <script>
-     function openSlideMenu() {
-                    document.getElementById('menu').style.width = '15%';
-                }
+    $(document).ready(function () {
+        $('#mydatatable').DataTable();
+    });
 
-                function closeSlideMenu() {
-                    document.getElementById('menu').style.width = '0';
-                    document.getElementById('content').style.marginLeft = '0';
-                }
+    function openSlideMenu() {
+        document.getElementById('menu').style.width = '15%';
+    }
 
-                $(document).ready(function () {
+    function closeSlideMenu() {
+        document.getElementById('menu').style.width = '0';
+        document.getElementById('content').style.marginLeft = '0';
+    }
 
-                    $('#sidebarCollapse').on('click', function () {
-                        $('#sidebar').toggleClass('active');
-                    });
-
-                });
+    $(document).ready(function () {
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+    });
 </script>
 
 </html>
--- Admin/projects.php
+++ Admin/projects.php
@@ -10,20 +10,29 @@
 <html>
 
 <head>
-    <title>NGCBDC</title>
+<title>NGCBDC</title>
     <link rel="icon" type="image/png" href="../Images/login2.png">
-    <link rel="stylesheet" type="text/css" href="../style.css">
-    <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
         integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script src="../JS/jquery/jquery-3.4.1.min.js"></script>
     <script src="../bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
-<div id="content">
+    <div id="content">
         <span class="slide">
             <a href="#" class="open" id="sideNav-a" onclick="openSlideMenu()">
                 <i class="fas fa-bars"></i>
@@ -138,9 +147,9 @@
                                             <h5>End Date: <?php echo $row[3] ;?></h5>
                                         </span>
                                         <input type="hidden" name="project_id" value="<?php echo $row[4];?>">
-                                        <button type="submit" class="btn btn-info" id="view-inventory-btn"
-                                            onclick="window.location.href='viewInventory.php'" name="viewInventory">View
-                                            inventory</button>
+                                        <button type="submit" class="btn btn-success" name="">Edit</button>
+                                        <button type="submit" class="btn btn-danger" name="" data-toggle="modal"
+                                            data-target="#close-project-modal"> Close Project</button>
                                     </div>
                                 </div>
                             </div>
@@ -198,6 +207,28 @@
             </div>
         </div>
     </section>
+
+    <div class="modal fade" id="close-project-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <h4>Are you sure you want to close this project?</h4>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    NAME NG PROJECT
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success">Yes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
 <script>
