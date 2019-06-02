f38df7dddf8b56b82d7abd777ee861dad9157b4a
jamspicaa <Jamspica25@gmail.com>
2019-06-02 11:37:32 +0800

    Update projects.php
    modal of edit

--- Admin/projects.php
+++ Admin/projects.php
@@ -10,7 +10,7 @@
 <html>
 
 <head>
-<title>NGCBDC</title>
+    <title>NGCBDC</title>
     <link rel="icon" type="image/png" href="../Images/login2.png">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
         integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
@@ -147,9 +147,10 @@
                                             <h5>End Date: <?php echo $row[3] ;?></h5>
                                         </span>
                                         <input type="hidden" name="project_id" value="<?php echo $row[4];?>">
-                                        <button type="submit" class="btn btn-success" name="">Edit</button>
+                                        <button type="button" class="btn btn-success" name="" data-toggle="modal"
+                                            data-target="#edit-project-modal">Edit</button>
                                         <button type="submit" class="btn btn-danger" name="" data-toggle="modal"
-                                            data-target="#close-project-modal"> Close Project</button>
+                                            data-target="#close-project-modal">Close Project</button>
                                     </div>
                                 </div>
                             </div>
@@ -224,6 +225,59 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-success">Yes</button>
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="edit-project-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <h4>PROJECT NAME</h4>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="needs-validation" novalidate>
+                        <div class="form-group">
+                            <label for="projectName" class="label-styles">Project Name</label>
+                            <input name="projectName" type="email" class="form-control"
+                                placeholder="Enter new project name" required>
+                            <div class="invalid-feedback">Please fill out this field.</div>
+                        </div>
+                        <div class="form-group">
+                            <label for="address" class="label-styles">Address:</label>
+                            <input name="address" type="text" class="form-control"
+                                placeholder="Enter new project address" required>
+                            <div class="invalid-feedback">Please fill out this field.</div>
+                        </div>
+                        <div class="form-group">
+                            <label for="startDate" class="label-styles">Start Date:</label>
+                            <input name="start_Date" type="date" class="form-control" required>
+                            <div class="invalid-feedback">Please fill out this field.</div>
+                        </div>
+                        <div class="form-group">
+                            <label for="endDate" class="label-styles">End Date:</label>
+                            <input name="end_Data" type="date" class="form-control" required>
+                            <div class="invalid-feedback">Please fill out this field.</div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="inputGroupSelect01">Materials Engineer Involved</label>
+                            </div>
+                            <select class="custom-select" id="inputGroupSelect01">
+                                <option selected>Choose...</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success">Save</button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
 
                 </div>
             </div>
