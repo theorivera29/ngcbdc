c738e4b49c6d9d997e30d3b0a88ff7c341f379ae
jamspicaa <Jamspica25@gmail.com>
2019-06-03 14:19:43 +0800

    Create addProject.php

---
+++ Admin/addProject.php
@@ -0,0 +1,167 @@
+<?php
+    include "../db_connection.php";
+    session_start();
+
+    $accounts_id = $_SESSION['account_id'];
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>NGCBDC</title>
+    <link rel="icon" type="image/png" href="../Images/login2.png">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script src="../js/jquery/jquery-3.4.1.min.js"></script>
+    <script src="../js/popper/popper.min.js"></script>
+    <script src="../bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.min.js">
+    </script>
+</head>
+
+<body>
+    <div id="content">
+        <span class="slide">
+            <a href="#" class="open" id="sideNav-a" onclick="openSlideMenu()">
+                <i class="fas fa-bars"></i>
+            </a>
+            <h4 class="title">NEW GOLDEN CITY BUILDERS AND DEVELOPMENT CORPORATION</h4>
+            <div class="account-container">
+                <?php 
+                        $sql = "SELECT * FROM accounts WHERE accounts_id = '$accounts_id'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_row($result);
+            ?>
+                <h5 class="active-user">
+                    <?php echo $row[1]." ".$row[2]; ?>
+                </h5>
+                <div class="btn-group dropdown-account">
+                    <button type="button" class="btn dropdown-toggle dropdown-settings" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="account.php">Account Settings</a>
+                        <a class="dropdown-item" href="">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </span>
+
+        <div id="menu" class="navigation sidenav">
+            <a href="#" class="close" id="sideNav-a" onclick="closeSlideMenu()">
+                <i class="fas fa-times"></i>
+            </a>
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <img src="../Images/login2.png" id="ngcbdc-logo">
+                </div>
+                <ul class="list-unstyled components">
+                    <li>
+                        <a href="dashboard.php" id="sideNav-a">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <a href="#accountSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
+                            id="sideNav-a">Account</a>
+                        <ul class="collapse list-unstyled" id="accountSubmenu">
+                            <li>
+                                <a href="accountcreation.php" id="sideNav-a">Create Account</a>
+                            </li>
+                            <li>
+                                <a href="listofaccounts.php" id="sideNav-a">List of Accounts</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="passwordrequest.php" id="sideNav-a">Password Request</a>
+                    </li>
+                    <li>
+                        <a href="projects.php" id="sideNav-a">Projects</a>
+                    </li>
+                    <li>
+                        <a href="logs.php" id="sideNav-a">Logs</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="add-project-container">
+        <form class="needs-validation" novalidate>
+            <div class="form-group">
+                <label for="projectName" class="label-styles">PROJECT NAME:</label>
+                <input name="projectName" type="email" class="form-control" placeholder="Enter project name" required>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="form-group">
+                <label for="address" class="label-styles">ADDRESS:</label>
+                <input name="address" type="text" class="form-control" placeholder="Enter project address" required>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="form-group">
+                <label for="startDate" class="label-styles">START DATE:</label>
+                <input name="start_Date" type="date" class="form-control" required>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="form-group">
+                <label for="endDate" class="label-styles">END DATE:</label>
+                <input name="end_Data" type="date" class="form-control" required>
+                <div class="invalid-feedback">Please fill out this field.</div>
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="inputGroupSelect01">Materials Engineer
+                        Involved</label>
+                </div>
+                <select id="multiselect" multiple="multiple">
+                    <option>JAM SPICA ROCAFORT</option>
+                </select>
+            </div>
+        
+
+        <div class="add-project-btn">
+            <button type="button" class="btn btn-success">Save</button>
+            <input type="reset" class="btn btn-danger" value="Cancel">
+        </div>
+        </form>
+    </div>
+</body>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#mydatatable').DataTable();
+
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+    });
+
+    function openSlideMenu() {
+        document.getElementById('menu').style.width = '15%';
+    }
+
+    function closeSlideMenu() {
+        document.getElementById('menu').style.width = '0';
+        document.getElementById('content').style.marginLeft = '0';
+    }
+
+    $(document).ready(function () {
+        $('#multiselect').multiselect({
+            buttonWidth: '75%',
+            includeSelectAllOption: true,
+            nonSelectedText: 'Select materials engineer'
+        });
+    });
+</script>
+
+</html>
