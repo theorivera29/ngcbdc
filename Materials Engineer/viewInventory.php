cc931b1b3758bbc24569151e5c8b5bb663e27f93
Caryl Marie <2161397@slu.edu.ph>
2019-06-03 14:25:32 +0800

    update

--- Materials Engineer/viewInventory.php
+++ Materials Engineer/viewInventory.php
@@ -27,7 +27,7 @@
 </head>
 
 <body>
-    <div id="content">
+<div id="content">
         <span class="slide">
             <a href="#" class="open" onclick="window.location.href='projects.php'">
                 <i class="fas fa-arrow-circle-left"></i>
@@ -210,24 +210,8 @@
 <script type="text/javascript">
     $(document).ready(function () {
         $('#mydatatable').DataTable();
-
-        $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
-
-        });
     });
 
-    function openSlideMenu() {
-        document.getElementById('menu').style.width = '15%';
-    }
-
-    function closeSlideMenu() {
-        document.getElementById('menu').style.width = '0';
-        document.getElementById('content').style.marginLeft = '0';
-    }
-
-    });
 </script>
-
 
 </html>
