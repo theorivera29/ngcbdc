c438859a0aaf1ab3c18fa3fd3657de1fbadf3f4d
jamspicaa <Jamspica25@gmail.com>
2019-06-03 16:05:44 +0800

    Files:
    adding of new materials
    add materials

---
+++ Materials Engineer/addingOfNewMaterials.php
@@ -0,0 +1,230 @@
+<?php
+    include "../db_connection.php";
+    session_start();
+
+    $accounts_id = $_SESSION['account_id'];
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>NGCBDC</title>
+    <link rel="icon" type="image/png" href="../Images/login2.png">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" href="../bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script src="../js/jquery/jquery-3.4.1.min.js"></script>
+    <script src="../js/popper/popper.min.js"></script>
+    <script src="../bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+</head>
+
+<body>
+    <div id="content">
+        <span class="slide">
+            <a href="#" class="open" onclick="openSlideMenu()">
+                <i class="fas fa-bars"></i>
+            </a>
+            <h4 class="title">NEW GOLDEN CITY BUILDERS AND DEVELOPMENT CORPORATION</h4>
+            <div class="account-container">
+                <?php 
+                        $sql = "SELECT * FROM accounts WHERE accounts_id = '$accounts_id'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_row($result);
+            ?>
+                <h5 class="active-user">
+                    <?php echo $row[1]." ".$row[2]; ?>
+                </h5>
+                <div class="btn-group dropdown-account">
+                    <button type="button" class="btn dropdown-toggle dropdown-settings" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="account.php">Account Settings</a>
+                        <a class="dropdown-item" href="../logout.php">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </span>
+
+        <div id="menu" class="navigation sidenav">
+            <a href="#" class="close" onclick="closeSlideMenu()">
+                <i class="fas fa-times"></i>
+            </a>
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <img src="../Images/login2.png" id="ngcbdc-logo">
+                </div>
+                <ul class="list-unstyled components">
+                    <li>
+                        <a href="dashboard.php" id="sideNav-a">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <a href="#siteSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
+                            id="sideNav-a">Site</a>
+                        <ul class="collapse list-unstyled" id="siteSubmenu">
+                            <li>
+                                <a href="projects.php" id="sideNav-a">Projects</a>
+                            </li>
+                            <li>
+                                <a href="sitematerials.php" id="sideNav-a">Site Materials</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="active">
+                        <a href="#haulingSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
+                            id="sideNav-a">Hauling</a>
+                        <ul class="collapse list-unstyled" id="haulingSubmenu">
+                            <li>
+                                <a href="fillouthauling.php" id="sideNav-a">Fill out Hauling Receipt</a>
+                            </li>
+                            <li>
+                                <a href="hauleditems.php" id="sideNav-a">View Hauled Materials</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="returnsOrReplaced.php" id="sideNav-a">Returns/Replacements</a>
+                    </li>
+                    <li>
+                        <a href="addingOfMaterials.php" id="sideNav-a">Adding of Materials</a>
+                    </li>
+                    <li>
+                        <a href="reports.php" id="sideNav-a">Reports</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+    <section id="tabs">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 project-tabs">
+                    <nav>
+                        <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
+                                role="tab" aria-controls="nav-home" aria-selected="true">ADD NEW CATEGORY</a>
+                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
+                                role="tab" aria-controls="nav-profile" aria-selected="false">ADD NEW MATERIAL</a>
+                        </div>
+                    </nav>
+                </div>
+                <div class="adding-of-materials-content">
+                    <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active adding-of-materials-container" id="nav-home"
+                            role="tabpanel" aria-labelledby="nav-home-tab">
+                            <table class="table new-category-table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Category</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="add-category">
+                                </tbody>
+                                <tfoot>
+                                    <tr id="haulingRow">
+                                        <td><input class="form-control" name="quantity" type="text" id="quantity"
+                                                placeholder="Category Name">
+                                        </td>
+                                        <td colspan="5">
+                                            <input type="button" class="btn btn-md btn-outline-secondary add-row"
+                                                value="Add Row" />
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                            <table class="table new-category-table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Category</th>
+                                        <th scope="col">Material</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="add-category">
+                                </tbody>
+                                <tfoot>
+                                    <tr id="haulingRow">
+                                        <td><select class="custom-select" id="inputGroupSelect01">
+                                                <option selected>Choose Category</option>
+                                                <option value=""></option>
+                                            </select>
+                                        </td>
+                                        <td><input class="form-control" name="quantity" type="text" id="quantity"
+                                                placeholder="Material Name">
+                                        </td>
+                                        <td colspan="5">
+                                            <input type="button" class="btn btn-md btn-outline-secondary add-row"
+                                                value="Add Row" />
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#mydatatable').DataTable();
+
+        $('table.display').DataTable();
+
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+    });
+
+    function openSlideMenu() {
+        document.getElementById('menu').style.width = '15%';
+    }
+
+    function closeSlideMenu() {
+        document.getElementById('menu').style.width = '0';
+        document.getElementById('content').style.marginLeft = '0';
+    }
+
+    $(document).ready(function () {
+        $(".add-row").click(function () {
+            var quantity = $("#quantity").val();
+            var unit = $("#unit").val();
+            var articles = $("#articles").val();
+            var markup = "<tr><td>" + quantity + "</td><td>" + articles + "</td><td>" +
+                unit +
+                "</td><td><input type='button' class='btn btn-sm btn-outline-secondary delete-row' value='Delete' /></td></tr>";
+            if ((quantity != '') && (articles != '') && (unit != '')) {
+                $("table tbody").append(markup);
+                $("#haulingRow input[type=text]").val('');
+                $("#haulingRow select").val('');
+            }
+        });
+
+        $("#haulingTable").on('click', '.delete-row', function () {
+            $(this).closest('tr').remove();
+        });
+
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+    });
+</script>
+
+
+</html>
