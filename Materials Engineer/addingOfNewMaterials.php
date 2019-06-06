0f83d80c0828c79863b922bd724a1c6948a2f5fe
benzservidad <2163227@slu.edu.ph>
2019-06-06 11:14:38 +0800

    Update addingOfNewMaterials.php

--- Materials Engineer/addingOfNewMaterials.php
+++ Materials Engineer/addingOfNewMaterials.php
@@ -156,7 +156,7 @@
                                 <div class="row form-group save-btn-container">
                                     <div class="col-lg-12">
                                         <input type="submit" class="btn btn-primary" value="Save Changes">
-                                        <input type="reset" class="btn btn-secondary" value="Cancel">
+                                        <input type="reset" class="btn btn-danger" value="Cancel">
                                     </div>
                                 </div>
 
