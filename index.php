2cfe6684d119145feb63470f3a8d5df898d33a4b
benzservidad <2163227@slu.edu.ph>
2019-06-06 11:03:54 +0800

    Update index.php
    Insert the link of the forgot password, an change the icon above

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 <html>
 
 <head>
-    <title>NGCBDC</title>
-    <link rel="icon" type="image/png" href="Images/NGCB_logo.png">
+<title>NGCBDC</title>
+    <link rel="icon" type="image/png" href="Images/login2.png">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -38,7 +38,7 @@
                 <div class="invalid-feedback">Please fill out this field.</div>
             </div>
             <div>
-                <p class="form-group forgot-password-link"><a href="#">Forgot your password?</a></p>
+                <p class="form-group forgot-password-link"><a href="forgotPassword.php">Forgot your password?</a></p>
             </div>
             <div class="form-group">
                 <p>
