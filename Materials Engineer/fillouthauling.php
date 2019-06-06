6bfc794d75ba5cde19940fdfc565e4b5a5b0e582
tuguinaymikka <2164725@slu.edu.ph>
2019-06-07 04:44:15 +0800

    patterns

--- Materials Engineer/fillouthauling.php
+++ Materials Engineer/fillouthauling.php
@@ -132,7 +132,7 @@
                                                 <label class="col-lg-12 col-form-label">Form No.:</label>
                                                 <div class="col-lg-12">
                                                     <input class="form-control" type="text" name="formNo"
-                                                          required>
+                                                          pattern="[0-9]*" title="Input numbers" required>
                                                 </div>
                                             </div>
                                         </div>
@@ -148,14 +148,14 @@
                                             <label class="col-lg-2 col-form-label">Deliver to:</label>
                                             <div class="col-lg-9">
                                                 <input class="form-control" type="text" name="deliverTo"
-                                                     required>
+                                                    pattern="[A-Za-z\s]*" title="Input letters" required>
                                             </div>
                                         </div>
                                         <div class="form-group row col-lg-12">
                                             <label class="col-lg-2 col-form-label">Hauled from:</label>
                                             <div class="col-lg-9">
                                                 <input class="form-control" type="text" name="hauledFrom"
-                                                     required>
+                                                     pattern="[A-Za-z\s]*" title="Input letters" required>
                                             </div>
                                         </div>
                                         <div class="card">
