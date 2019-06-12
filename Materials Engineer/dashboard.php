5616032c5ae51eb943914c1790c41d991d5b2944
jamspicaa <Jamspica25@gmail.com>
2019-06-13 02:34:51 +0800

    Update dashboard.php

--- Materials Engineer/dashboard.php
+++ Materials Engineer/dashboard.php
@@ -260,7 +260,7 @@
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-success">Yes</button>
-                                            <button type="button" class="btn btn-secondary"
+                                            <button type="button" class="btn btn-danger"
                                                 data-dismiss="modal">No</button>
 
                                         </div>
