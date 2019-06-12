46b946bd6fec431d692640eff20bb5ce2636503b
jamspicaa <Jamspica25@gmail.com>
2019-06-12 22:10:33 +0800

    Update previousReportsPage.php
    Removed navbar

--- Materials Engineer/previousReportsPage.php
+++ Materials Engineer/previousReportsPage.php
@@ -24,7 +24,7 @@
 </head>
 
 <body>
-<div id="content">
+    <div id="content">
         <span class="slide">
             <a href="#" class="open" id="sideNav-a" onclick="openSlideMenu()">
                 <i class="fas fa-bars"></i>
@@ -88,109 +88,8 @@
                         </ul>
                     </li>
                     <li class="active">
-                        <a href="#transactionSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
-                            id="sideNav-a">Transactions</a>
-                        <ul class="collapse list-unstyled" id="transactionSubmenu">
-                            <li>
-                                <a href="requisitionslip.php" id="sideNav-a">Material Requisition Slip</a>
-                            </li>
-                            <li>
-                                <a href="deliveredin.php" id="sideNav-a">Delivered In Form</a>
-                            </li>
-                            <li>
-                                <a href="viewTransactions.php" id="sideNav-a">View Transactions</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="returns.php" id="sideNav-a">Returns</a>
-                    </li>
-                    <li>
-                        <a href="addingOfNewMaterials.php" id="sideNav-a">Adding of Materials</a>
-                    </li>
-                    <li class="active">
-                        <a href="#reportSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
-                            id="sideNav-a">Reports</a>
-                        <ul class="collapse list-unstyled" id="reportSubmenu">
-                            <li>
-                                <a href="currentReport.php" id="sideNav-a">Monthly Report</a>
-                            </li>
-                            <li>
-                                <a href="previousReports.php" id="sideNav-a">Previous Reports</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-
-        </div>
-
-    </div><div id="content">
-        <span class="slide">
-            <a href="#" class="open" id="sideNav-a" onclick="openSlideMenu()">
-                <i class="fas fa-bars"></i>
-            </a>
-            <h4 class="title">NEW GOLDEN CITY BUILDERS AND DEVELOPMENT CORPORATION</h4>
-            <div class="account-container">
-                <?php 
-                        $sql = "SELECT * FROM accounts WHERE accounts_id = '$accounts_id'";
-                        $result = mysqli_query($conn, $sql);
-                        $row = mysqli_fetch_row($result);
-            ?>
-                <h5 class="active-user">
-                    <?php echo $row[1]." ".$row[2]; ?>
-                </h5>
-                <div class="btn-group dropdown-account">
-                    <button type="button" class="btn dropdown-toggle dropdown-settings" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a class="dropdown-item" href="account.php">Account Settings</a>
-                        <a class="dropdown-item" href="../logout.php">Logout</a>
-                    </div>
-                </div>
-            </div>
-        </span>
-
-        <div id="menu" class="navigation sidenav">
-            <a href="#" class="close" id="sideNav-a" onclick="closeSlideMenu()">
-                <i class="fas fa-times"></i>
-            </a>
-            <nav id="sidebar">
-                <div class="sidebar-header">
-                    <img src="../Images/login2.png" id="ngcbdc-logo">
-                </div>
-                <ul class="list-unstyled components">
-                    <li>
-                        <a href="dashboard.php" id="sideNav-a">Dashboard</a>
-                    </li>
-                    <li class="active">
-                        <a href="#siteSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
-                            id="sideNav-a">Site</a>
-                        <ul class="collapse list-unstyled" id="siteSubmenu">
-                            <li>
-                                <a href="projects.php" id="sideNav-a">Projects</a>
-                            </li>
-                            <li>
-                                <a href="sitematerials.php" id="sideNav-a">Site Materials</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="active">
-                        <a href="#haulingSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
-                            id="sideNav-a">Hauling</a>
-                        <ul class="collapse list-unstyled" id="haulingSubmenu">
-                            <li>
-                                <a href="fillouthauling.php" id="sideNav-a">Fill out Hauling Receipt</a>
-                            </li>
-                            <li>
-                                <a href="hauleditems.php" id="sideNav-a">View Hauled Materials</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="active">
-                        <a href="#transactionSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"
-                            id="sideNav-a">Transactions</a>
+                        <a href="#transactionSubmenu" data-toggle="collapse" aria-expanded="false"
+                            class="dropdown-toggle" id="sideNav-a">Transactions</a>
                         <ul class="collapse list-unstyled" id="transactionSubmenu">
                             <li>
                                 <a href="requisitionslip.php" id="sideNav-a">Material Requisition Slip</a>
@@ -227,7 +126,7 @@
         </div>
 
     </div>
-
+    
     <div class="hauled-items-container">
         <h4 class="card-header">PROJECT NAME Previous Reports</h4>
         <table class="table hauled-items-table table-striped table-bordered" id="mydatatable">
@@ -251,19 +150,19 @@
                         $result = mysqli_query($conn, $sql);
                         while($row = mysqli_fetch_array($result)) {
                     ?>
-                        <tr>
-                            <form action="../server.php" method="POST">
-                                <td><?php echo $month_name = date("F", mktime(0, 0, 0, $row[0], 10)); ?></td>
-                                <td><?php echo $row[1]?></td>
-                                <td>
-                                    <input type="hidden" name="projects_id" value="<?php echo $projects_id ;?>">
-                                    <input type="hidden" name="lastmatinfo_month" value="<?php echo $row[0] ;?>">
-                                    <input type="hidden" name="lastmatinfo_year" value="<?php echo $row[1] ;?>">
-                                    <button type="submit" class="btn btn-success" name="viewPrevReport">View</button>
-                                </td>
-                            </form>
-                        </tr>
-                    <?php
+                <tr>
+                    <form action="../server.php" method="POST">
+                        <td><?php echo $month_name = date("F", mktime(0, 0, 0, $row[0], 10)); ?></td>
+                        <td><?php echo $row[1]?></td>
+                        <td>
+                            <input type="hidden" name="projects_id" value="<?php echo $projects_id ;?>">
+                            <input type="hidden" name="lastmatinfo_month" value="<?php echo $row[0] ;?>">
+                            <input type="hidden" name="lastmatinfo_year" value="<?php echo $row[1] ;?>">
+                            <button type="submit" class="btn btn-success" name="viewPrevReport">View</button>
+                        </td>
+                    </form>
+                </tr>
+                <?php
                     }
                 ?>
             </tbody>
