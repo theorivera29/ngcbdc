61c8b5024cadf297b05d326766e9fdb04931d21c
2166538 <2166538>
2019-06-15 01:11:28 +0800

    deliveredin reqslip hauling

--- Materials Engineer/fillouthauling.php
+++ Materials Engineer/fillouthauling.php
@@ -174,7 +174,6 @@
                                                     <tr>
                                                         <th scope="col">Quantity</th>
                                                         <th scope="col">Articles</th>
-                                                        <th scope="col">Unit</th>
                                                         <th scope="col">Action</th>
                                                     </tr>
                                                 </thead>
@@ -199,7 +198,7 @@
                                                                                 $result = mysqli_query($conn, $sql);
                                                                                 while ($row = mysqli_fetch_row($result)) {
                                                                         ?>
-                                                                    <option value="<?php echo $row[0]; ?>">
+                                                                    <option value="<?php echo $row[1]; ?>">
                                                                         <?php echo $row[0]; ?>
                                                                     </option>
                                                                     <?php
@@ -207,11 +206,7 @@
                                                                         ?>
                                                                 </select>
                                                             </div>
-                                                        </td>
-                                                        <td><input class="form-control" name="unit[]" pattern="[A-Za-z\s]*" title="Input letters" type="text" id="unit" placeholder="Unit" required>
-                                                            <div class="invalid-feedback">Please fill out this field.
-                                                            </div>
-                                                        </td>
+                                                        </td><input type="hidden" class="form-control" name="unit[]" pattern="[A-Za-z\s]*" title="Input letters" type="text" id="unit" placeholder="Unit" required>
                                                         <td colspan="5">
                                                             <input type="button" class="btn btn-md btn-outline-secondary add-row" value="Add Row" />
                                                         </td>
@@ -359,7 +354,6 @@
                                                         <tr>
                                                             <th scope="col">Quantity</th>
                                                             <th scope="col">Articles</th>
-                                                            <th scope="col">Unit</th>
                                                             <th scope="col">Action</th>
                                                         </tr>
                                                     </thead>
@@ -384,7 +378,7 @@
                                                                                 $result = mysqli_query($conn, $sql);
                                                                                 while ($row = mysqli_fetch_row($result)) {
                                                                         ?>
-                                                                        <option value="<?php echo $row[0]; ?>">
+                                                                        <option value="<?php echo $row[1]; ?>">
                                                                             <?php echo $row[0]; ?>
                                                                         </option>
                                                                         <?php
@@ -392,11 +386,7 @@
                                                                         ?>
                                                                     </select>
                                                                 </div>
-                                                            </td>
-                                                            <td><input class="form-control" name="unit[]" pattern="[A-Za-z\s]*" title="Input letters" type="text" id="unit1" placeholder="Unit" required>
-                                                                <div class="invalid-feedback">Please fill out this
-                                                                    field.</div>
-                                                            </td>
+                                                            </td><input type="hidden" class="form-control" name="unit[]" pattern="[A-Za-z\s]*" title="Input letters" type="text" id="unit1" placeholder="Unit" required>
                                                             <td colspan="5">
                                                                 <input type="button" class="btn btn-md btn-outline-secondary add-row1" value="Add Row" />
                                                             </td>
@@ -505,6 +495,8 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </section>
 </body>
 <script type="text/javascript">
     $(document).ready(function() {
@@ -513,11 +505,11 @@
             var quantity = $("#quantity").val();
             var unit = $("#unit").val();
             var articles = $("#articles").val();
-            var markup = "<tr><td><input type='text' class='form-control' value='" +
+            var markup = "<tr><td><input type='text' name='quantity[]' class='form-control' value='" +
                 quantity +
-                "' required/></td><td><input type='text' class='form-control' value='" +
+                "' required/></td><td><input type='text' name='articles[]' class='form-control' value='" +
                 articles +
-                "' required/></td><td><input type='text' class='form-control' value='" +
+                "' required/><input type='hidden' name='unit[]' class='form-control' value='" +
                 unit +
                 "' required/></td><td><input type='button' class='btn btn-sm btn-outline-secondary delete-row' value='Delete' /></td></tr>";
             if ((quantity != '') && (articles != '') && (unit != '')) {
@@ -536,11 +528,11 @@
             var quantity1 = $("#quantity1").val();
             var unit1 = $("#unit1").val();
             var articles1 = $("#articles1").val();
-            var markup1 = "<tr><td><input type='text' class='form-control' value='" +
+            var markup1 = "<tr><td><input type='text' name='quantity[]' class='form-control' value='" +
                 quantity1 +
-                "' required/></td><td><input type='text' class='form-control' value='" +
+                "' required/></td><td><input type='text' name='articles[]' class='form-control' value='" +
                 articles1 +
-                "' required/></td><td><input type='text' class='form-control' value='" +
+                "' required/><input type='hidden' name='unit[]' class='form-control' value='" +
                 unit1 +
                 "' required/></td><td><input type='button' class='btn btn-sm btn-outline-secondary delete-row' value='Delete' /></td></tr>";
             if ((quantity1 != '') && (articles1 != '') && (unit1 != '')) {
@@ -558,6 +550,15 @@
             $('#sidebar').toggleClass('active');
         });
     });
+    
+            $('#articles').on('change', function () {
+            console.log($(this).children('option:selected').val())
+            $.get('http://localhost/NGCBDC/Materials%20Engineer/../server.php?mat_name=' + $(this).children(
+                'option:selected').val(), function (data) {
+                var d = JSON.parse(data);
+                $('#unit').val(d[0][0])
+            })
+        })
 
     (function() {
         'use strict';
